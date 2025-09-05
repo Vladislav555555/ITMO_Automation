5c8ebd76c1814b5c468357550d8e04f2db23b68f
Vladislav <Hearthstoneganster@gmail.com>
2025-09-05 14:28:50 +0300

    First commit

---
+++ Homework/Task 2.1.py
@@ -0,0 +1,12 @@
+def task_1()-> None:
+    a: int = 150
+    b: float = 3.7
+    c: str = "Homework"
+    d: list = [1,2,3,5,7,9]
+    e: bool = True
+    print(f"Переменная a относится к классу {type(a)}")
+    print(f"Переменная b относится к классу {type(b)}")
+    print(f"Переменная c относится к классу {type(c)}")
+    print(f"Переменная d относится к классу {type(d)}")
+    print(f"Переменная e относится к классу {type(e)}")
+task_1()
