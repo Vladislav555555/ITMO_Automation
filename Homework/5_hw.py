904b621a797c2036c5485f351418c83821f901ea
Vladislav <Hearthstoneganster@gmail.com>
2025-09-15 21:49:00 +0300

    First commit

--- Homework/5_hw.py
+++ Homework/5_hw.py
@@ -1,22 +1,4 @@
-'''from selenium.webdriver.common.by import By
-from selenium import webdriver
 
-
-driver = webdriver.Chrome()
-driver.get("https://www.saucedemo.com/")
-
-user_name= driver.find_element(By. CSS_SELECTOR,'#user-name' )
-password = driver.find_element(By. CSS_SELECTOR,'#password' )
-login_button = driver.find_element(By. CSS_SELECTOR,'#login-button' )
-
-
-if user_name and password and login_button is None:
-    print("Поле не найдено!")
-else:
-    print("Поле успешно найдено!")
-'''
-
-# Разбирался с try и except. Без них при отсутствии элемента код ломается
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
